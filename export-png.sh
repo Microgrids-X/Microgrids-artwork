6578ab9ff93edc843ed883b3e8eacb798c758e60
Pierre Haessig <pierre.haessig@crans.org>
2022-08-19 10:04:37 +0200

    add square icon for Microgrids-X

--- export-png.sh
+++ export-png.sh
@@ -4,6 +4,9 @@
 
 for n in jl m py web X
 do
-    echo exporting $n icon...
+    echo exporting $n logo...
     inkscape --export-area-page --export-dpi=384 --export-filename=png/Microgrids-$n-256.png svg/Microgrids-$n.svg
 done
+
+echo exporting X icon...
+inkscape --export-area-page --export-dpi=768 --export-filename=png/Microgrids-X-icon-512.png svg/Microgrids-X-icon.svg
